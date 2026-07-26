79c89651dea3d06075a205d2cb60be781ccdc84e
Akhlys97 <salihdadali7@gmail.com>
2026-07-26 16:21:47 +0300

    sanity check

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { db } from "@/lib/firebase";
 import { auth } from "@/lib/firebase";
 
 export default function Home() {
-  console.log(db, auth);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
