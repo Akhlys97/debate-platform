ac67d205851c6aa6fc25558d61f03df6c68c1cde
Akhlys97 <salihdadali7@gmail.com>
2026-08-23 19:58:07 +0300

    Join country function

--- lib/communities.ts
+++ lib/communities.ts
@@ -1,7 +1,9 @@
-import { CommunityFirestoreData, CommunityJSON, CommunityType, convertCommunityFirestoreData, EntranceType } from "@/types/community";
-import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { CommunityFirestoreData, CommunityJSON, CommunityType, convertCommunityFirestoreData, EntranceType, Rank } from "@/types/community";
+import { addDoc, arrayUnion, collection, doc, getDoc, runTransaction, Transaction } from "firebase/firestore";
 import { db } from "./firebase";
 import { convertFirestoreData, UserFirestoreData } from "@/types/user";
+import { getCode } from "country-list";
+import { Role } from "@/types/user";
 
 export async function createCommunity(
     name: string,
@@ -54,5 +56,42 @@ export async function getUserCommunities(uid: string): Promise<(CommunityJSON & 
     else{
         throw new Error("User not found");
     }
+}
+
+export function getCountryCommunityId(country: string): string{
+    const code = getCode(country)
+    if (!code){
+        throw new Error(`No country code found for: ${country}`);
+    }
+    const prefix: string = "country_";
+    return prefix + code;
+}
+
+export async function joinCountryCommunity(uid: string, country: string, role: Role){
+    const communityId: string = getCountryCommunityId(country);
+    const rank: Rank = (role === "institutional_account"? "senior" : "member");    
+    await runTransaction(db, async (transaction) => {
+        const communityDocRef = doc(db, "communities", communityId);
+        const docSnap = await transaction.get(communityDocRef);
+        if (!docSnap.exists()){
+            transaction.set(communityDocRef, {
+                name: country,
+                description: "",
+                type: "country",
+                entranceType: "open",
+                founderId: "system",
+                isTeamSociety: false,
+                creationDate: new Date(),
+            });
+        }
+        const membershipDocRef = doc(db, "communities", communityId, "members", uid);
+        transaction.set(membershipDocRef, {
+            uid: uid,
+            rank: rank,
+            joinedAt: new Date(),
+        });
+        const userDocRef = doc(db, "users", uid);
+        transaction.update(userDocRef, { communityIds: arrayUnion(communityId) });
+    });
 }
 
